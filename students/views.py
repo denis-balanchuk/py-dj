6cd8028db970dd5f2e2380c78f467851313ccad8
denis-balanchuk <prostoyaschik@mail.ua>
2016-01-05 16:22:56 +0200

    dynamix groups

--- students/views.py
+++ students/views.py
@@ -27,7 +27,7 @@ def students_list(request):
 	return render(request, 'students/students_list.html', {'students': students})
 
 def students_add(request):
-	return HttpResponse('<h1>students Add Form</h1>')
+	return HttpResponse('<h1>Students Add Form</h1>')
 
 def students_edit(request, sid):
 	return HttpResponse('<h1>Edit Student %s</h1>' % sid)
@@ -37,8 +37,25 @@ def students_delete(request, sid):
 
 #Views for Groups
 
+#def groups_list(request):
+#	return HttpResponse('<h1>Groups Listing</h1>')
+
 def groups_list(request):
-	return HttpResponse('<h1>Groups Listing</h1>')
+	groups = (
+		{'id': 1,
+		 'name_of_group': u'МтМ-21',
+		 'starosta': u'Ячмінов Олег',
+		 },
+		{'id': 2,
+		 'name_of_group': u'МтМ-22',
+		 'starosta': u'Віталій Подоба',
+		 },
+		{'id': 3,
+		 'name_of_group': u'МтМ-23',
+		 'starosta': u'Назаров Андрій',
+		 },
+		)
+	return render(request, 'students/groups_list.html', {'groups': groups})
 
 def groups_add(request):
 	return HttpResponse('<h1>Group Add Form</h1>')
@@ -46,6 +63,6 @@ def groups_add(request):
 def groups_edit(request, gid):
 	return HttpResponse('<h1>Edit Group %s</h1>' % gid)
 
-def groups_delete(request, git):
+def groups_delete(request, gid):
 	return HttpResponse('<h1>Delete Group %s</h1>' % gid)
 
